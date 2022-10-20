e59a9ea1bb35dca809caa0afe7f22413b0611181
chrissietina92 <56897522+chrissietina92@users.noreply.github.com>
2022-10-20 16:49:57 +0100

    SQL file for the creation of the GOL_users database

---
+++ GOL_users.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE GOL_users;
+USE GOL_users;
+
+CREATE TABLE the_users (
+    UserID int,
+    FirstName varchar(65),
+	LastName varchar(65),
+    Email varchar(65),
+    DOB DATE,
+    City varchar(65),
+    Username varchar(65),
+	UserPassword varchar(65)
+);
+
+
+INSERT INTO the_users (UserID, FirstName, LastName, Email, DOB, City, Username, UserPassword)
+VALUES (1, 'Jake', 'Callow', 'jcal@email.com', str_to_date('05-12-1992', '%d-%m-%Y'), 'London', 'cobrien1', 'cat123'),
+(2, 'Fred', 'Smith', 'fsmith@email.com', 1995-02-19, 'Bristol', 'freddy95', 'bristol11'),
+(3, 'Hayley', 'Bieber', 'hayley99@email.com', 1997-09-01, 'Cambridge', 'hbieber1997', 'ilovejustin1'),
+(4, 'Luciano', 'Sovino', 'lucosovino89@email.com', 1989-10-29, 'Cardiff', 'lucosov89', 'helloworld0');
