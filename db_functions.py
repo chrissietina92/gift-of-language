6685c7d4f782b0a3df934f1354702ce2a5ffd34a
chrissietina92 <56897522+chrissietina92@users.noreply.github.com>
2022-10-20 16:46:36 +0100

    Adding file with sql database functions

---
+++ db_functions.py
@@ -0,0 +1,81 @@
+import mysql.connector
+from config import USER, HOST, PASSWORD
+
+
+def _connect_to_db(db_name):
+    connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        auth_plugin='mysql_native_password',
+        database=db_name
+    )
+    return connection
+
+
+def does_user_exist(column, value):
+    db_connection = None
+    try:
+        db_name = 'GOL_users'
+        # Database engine
+        db_connection = _connect_to_db(db_name)
+        # Cursor
+        cur = db_connection.cursor()
+        print("Database connection Successful")
+
+        query = """
+                    SELECT (EXISTS(SELECT FirstName
+                    FROM the_users
+                    WHERE {COLUMN} = '{VALUE}'));
+                    """.format(COLUMN = column, VALUE = value)
+        cur.execute(query)
+        result = cur.fetchall()
+
+        # print(result)
+        ## THE ABOVE QUEUERY WILL EITHER RETURN
+        ## [(1,)] WHICH REPRESENTS TRUE
+        ## OR [(0,)] WHICH REPRESENTS FALSE
+
+        if result[0][0] == 1:
+            print('A user exists with these details')
+        else:
+            print('A user does not exist with these details')
+
+        cur.close()
+    except Exception:
+        raise DbConnectionError
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection closed")
+
+
+def add_a_new_user(userid, firstname, lastname, email, dob, city, username, password):
+    db_connection = None
+    try:
+        db_name = 'GOL_users'
+        # Database engine
+        db_connection = _connect_to_db(db_name)
+        # Cursor
+        cur = db_connection.cursor()
+        print("Database connection Successful")
+
+        query = """
+                INSERT INTO the_users (UserID, FirstName, LastName, Email, DOB, City, Username, UserPassword)
+                VALUES ('{USERID}', '{FIRSTNAME}', '{LASTNAME}', '{EMAIL}', str_to_date('{DOB}', '%d-%m-%Y'), '{CITY}', '{USERNAME}', '{PASSWORD}')
+                """.format(USERID=userid, FIRSTNAME=firstname, LASTNAME=lastname, EMAIL=email, DOB=dob,
+                           CITY=city, USERNAME=username, PASSWORD=password)
+        cur.execute(query)
+        db_connection.commit()
+        cur.close()
+
+    except Exception:
+        raise DbConnectionError
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection closed")
+
+
