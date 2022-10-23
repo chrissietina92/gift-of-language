9c8de1f5cc716b2adc535560206275268abbf49f
Daniella Omokore <daniellaomokore@gmail.com>
2022-10-23 16:38:04 +0100

    random generator word updated

---
+++ api.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_definition(word):
+    dictionary_url = 'https://api.dictionaryapi.dev/api/v2/entries/en/{}'.format(word)
+
+    response = requests.get(dictionary_url)
+    # print(response.status_code)
+    word_data = response.json()
+
+    # writing the word and its definition to a file
+    with open('SearchedWords.txt', 'w') as SearchedWords:
+
+        # word_data is in a form of a list of dictionaries.
+        # within each index, the values in the dictionaries are another list of dictionaries
+
+        for dictionary in word_data:  # i is each index in word_data
+            SearchedWords.writelines("\n\nWord: {}".format(word.capitalize()))
+            SearchedWords.writelines("\nDefinition: " + dictionary['meanings'][0]['definitions'][0]['definition'])
+            SearchedWords.writelines("\nExample: " + dictionary['meanings'][0]['definitions'][0]['example'])
+
+
+# example looking up the word 'House' on the API
+get_definition('house')
--- daily_word_automation/daily_words.py
+++ daily_word_automation/daily_words.py
@@ -2,33 +2,31 @@ from datetime import datetime
 import requests
 import random
 
-#from #rebeccas pythonfile import login_interface
+
+# from rebeccas pythonfile import login_interface
 
 
-# Userdefined exception
-class Value_not_in_twenty_four_hour(ValueError):
+# User-defined exception
+class ValueNotInTwentyFourHour(ValueError):
     # called if the time entered is not in 24hr time
     pass
 
 
 def set_reminder_time():
-
     try:
         print("Please enter the time you would like your daily reminder in 24hr format.")
         reminderHour = int(input("Enter the Hour : "))
         reminderMin = int(input("Enter the Minute : "))
 
         if 24 < reminderHour < 1 and 60 < reminderMin < 1:
-            raise Value_not_in_twenty_four_hour
-    
-    except Value_not_in_twenty_four_hour:
+            raise ValueNotInTwentyFourHour
+
+    except ValueNotInTwentyFourHour:
         print("Please enter your reminder time in 24hr format.")
 
-    else:          
-        if reminderHour == datetime.datetime.now().hour and reminderMin == datetime.now().minute: #if the current time is equal to the reminder time
+    else:
+        if reminderHour == datetime.datetime.now().hour and reminderMin == datetime.now().minute:  # If the current time is equal to the reminder time
             randomWordGenerator()
-              
-
 
 
 """if login_interface() == True:  # if user successfully logs in
@@ -36,41 +34,40 @@ def set_reminder_time():
     
 """
 
-# GENERATING THE RANDOM WORD FOR USERS DAILY WORD
-def randomWordGenerator():    
 
+# Documentation:
+# I'm using this manual method below for the random word generator based on the words in our chosen api
+# instead of using the random words library as :
+# 1. A lot of the words don#t exist in the api we are using so the definition and examples etc can't be searched
+# 2. The words it generates are extremely advanced english that i don't even know myself and aren't commonly used in day too day speaking
+
+# GENERATING THE RANDOM WORD FOR USERS DAILY WORD
+def randomWordGenerator():
     # Converting the text file of the dictionary words into a list of strings
     with open("daily_word_automation\english.txt", encoding="utf8") as wordDictionary:
-        wordDictionaryList = []       # the wordDictionary in list form
+        wordDictionaryList = []  # the wordDictionary in list form
         for line in wordDictionary:
             wordDictionaryList.append(line.strip)
-        #print(wordDictionaryList)    # the wordDictionary in list form
+        #  print(wordDictionaryList)    # the wordDictionary in list form
 
     # Total number of dictionary words
     dictionaryLength = len(wordDictionaryList)
-    #print(dictionaryLength)  
+    #  print(dictionaryLength)
 
     # generates a random number within the range of the index for the list of dictionary words
-    randomDictIndex = random.randint(0,dictionaryLength-1) 
-    #print(randomDictIndex)  
+    randomDictIndex = random.randint(0, dictionaryLength - 1)
+    #  print(randomDictIndex)
 
     # getting our random word
     randomWord = wordDictionaryList[int(randomDictIndex)]
-    
-    
-
-
-
 
 def searchAPIForRandomWord(randomWord):
-
     # CONNECTING TO AN API TO SEARCH WITH THE RANDOM WORD AND PRINT ITS NAME AND DEFINITION
     dictionary_url = 'https://api.dictionaryapi.dev/api/v2/entries/en/{}'.format(randomWord)
 
     response = requests.get(dictionary_url)
-    #print(response.status_code)  #should be 200
-    word_data = response.json()    # word_data is the API's list of dictionaries
-
+    # print(response.status_code)  #should be 200
+    word_data = response.json()  # word_data is the API's list of dictionaries
 
     """
     Explanation for below code:
@@ -90,14 +87,10 @@ def searchAPIForRandomWord(randomWord):
     """
 
     # printing the name and definition of the word from its dictionary
-    for dictionary in word_data:    
-        print(dictionary['word'])   # print the value of the word key
-        print(dictionary['meanings'][0]['definitions'][0]['definition'])  
+    for dictionary in word_data:
+        print("Word: " + dictionary['word'])  # print the value of the word key
+        print("Definition: " + dictionary['meanings'][0]['definitions'][0]['definition'])
+        #print("Definition: " + dictionary['meanings'][0]['definitions'][0]['example'])
 
 
-searchAPIForRandomWord("cat")
-
-
-
-
-
+#searchAPIForRandomWord("coffee")
