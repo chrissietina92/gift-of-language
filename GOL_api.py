0e15b733d4b8fab8e82f4c4870fdc188a80f6724
chrissietina92 <56897522+chrissietina92@users.noreply.github.com>
2022-10-24 17:28:01 +0100

    frontend
    adding more front end

--- GOL_api.py
+++ GOL_api.py
@@ -7,12 +7,30 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        form = request.form
+        get_details(form)
+        # print(form) #ImmutableMultiDict([('logintype', 'u'), ('username', 'hi'), ('password', 'Chrissie'), ('next', 'Next')])
     return render_template('login.html')
+
+def get_details(form):
+    chosen_log_in = form['logintype']
+    chosen_log_in_input = form['username']
+    password = form['password']
+
+
+
 
 @app.route('/signup')
 def signup():
+    if request.method == 'POST':
+        form = request.form
+        get_details(form)
     return render_template('signup.html')
 
 
