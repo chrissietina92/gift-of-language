c3773b9ef54221b5c658a1685dc0090b78dd96d0
Xhensjola <112581767+Xhensjola@users.noreply.github.com>
2022-10-26 20:36:43 +0100

    commited after fixing the issue with set_reminder never exiting

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-import Login_Interface_Python_Logic
+from Login_Interface_Python_Logic import existing_customer_check
 from db_functions import new_user_credentials
-from daily_words import set_reminder_time
+from daily_words import randomWordGenerator
 from db_searched_words import add_searched_word, display_all_searched_words, delete_searched_words_for_new_user, clean_db_for_new_user
 from api import show_word_and_definition
+import schedule
+import time
+
+def set_reminder_time():
+
+    reminderTime = input("Please enter the time you would like your daily reminder in 24hr format:")
+
+    # Every day at 'reminderTime' time randomWordGenerator() is called.
+    schedule.every().day.at("{}".format(reminderTime)).do(randomWordGenerator)
+    schedule.every().day.at("{}".format(reminderTime)).do(continue_learning)
+    # Loop so that the scheduling task
+    # keeps on running all time.
+    #learn_words()
+    while True:
+        # Checks whether a scheduled task
+        # is pending to run or not
+        schedule.run_pending()
+        time.sleep(1)
+
 
 
 def learn_words():
-    start = input('Would you like to search your Schedule your words, Search your Dictionary, or view your Searched Words? (Schedule Word/Search Dictionary//View Searched Words) ')
+    start = input('Would you like to search your Schedule your words, Search your Dictionary, or View your Searched Words? (Schedule Word/Search Dictionary/View Searched Words) ')
     if start == 'Schedule Word':
         set_reminder_time()
-        continue_learning()
+
     elif start == 'Search Dictionary':
         search_words_in_dictionary()
         continue_learning()
     elif start == 'View Searched Words':
         display_all_searched_words()
+        continue_learning()
+    else:
+        print('Please try again')
         continue_learning()
 
 
@@ -33,7 +55,7 @@ def continue_learning():
     cont_learning = input('Would you like to continue? (Yes/No) ')
     if cont_learning == 'No':
         print('See you next time.')
-        exit(1)
+        exit(0)
     elif cont_learning == 'Yes':
         learn_words()
 
@@ -49,7 +71,7 @@ def run():
                         Choice (1/2): """))
 
     if option == 1:
-        Login_Interface_Python_Logic.login_interface()
+        existing_customer_check()
         print("Let's start learning")
         learn_words()
 
