4ee70ffde2e6a259149dfcb3635ec0455a442b4a
Becky-AJ <68607075+Becky-AJ@users.noreply.github.com>
2022-10-31 21:06:57 +0000

    All are still in classes.
    Printing test statements removed.
    Tidier output.
    
    DB connections will need to become a decorator? @Cristina may need your help with using your decorator. Perhaps a separate decorator file should be created?
    
    Will create the unit tests now.

--- GOL_Streaks_Function(s).py
+++ GOL_Streaks_Function(s).py
@@ -2,6 +2,17 @@ import mysql.connector
 from config import USER, HOST, PASSWORD
 from datetime import datetime, timedelta, date
 
+def _connect_to_db(db_name):
+    #attribute
+    connection = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            auth_plugin='mysql_native_password',
+            database=db_name
+        )
+    return connection
+
 class TheUserStreak:
 
     def __init__(self, user_id_value, column, value):
@@ -21,7 +32,6 @@ class TheUserStreak:
             db_connection = _connect_to_db(db_name)
             # Cursor
             cur = db_connection.cursor()
-            print("Database connection Successful")
 
             # QUERY MADE TO MYSQL
             login_query = "SELECT DATE_FORMAT(Lastlogin, '%Y-%m-%d') FROM the_users WHERE {COLUMN} = '{VALUE}';".format(COLUMN=self.column, VALUE=self.value)
@@ -32,24 +42,21 @@ class TheUserStreak:
             # FOR LOOP TO EXTRACT THE DATA FROM TUPLE CASING.
             for data in lastlogindata:
                 self.last_login = data[0]
-            # TESTING WITH A PRINT STATEMENT
-            print("Your last login date was: {}".format(str(self.last_login)))
+
             # THIS CLAUSE IS A CONTINGENCY FOR USERS WHO ARE LOGGING IN FOR THE FIRST TIME.
             if self.last_login is None:
                 lastlogindtobj = datetime.now().date()
                 self.last_login = lastlogindtobj.strftime("%Y-%m-%d")
                 return False
             else:
-                print('We have your last login date')
                 return True
 
         except Exception:
-                raise ConnectionError
+            raise ConnectionError
 
         finally:
             if db_connection:
                 db_connection.close()
-                print("DB connection closed")
 
 
 
@@ -62,7 +69,6 @@ class TheUserStreak:
             db_connection = _connect_to_db(db_name)
             # Cursor
             cur = db_connection.cursor()
-            print("Database connection Successful")
 
             streaks_query_01 = "SELECT " \
                                "UserStreak " \
@@ -82,7 +88,6 @@ class TheUserStreak:
             else:
                 new_streaker = False
 
-            print("Streak before: {} Days.".format(self.existing_user_streak))
             return new_streaker
 
         except Exception:
@@ -91,7 +96,6 @@ class TheUserStreak:
         finally:
             if db_connection:
                 db_connection.close()
-                print("DB connection closed")
 
 
     def calculate_login_diff(self):
@@ -99,16 +103,12 @@ class TheUserStreak:
         d1 = datetime.now().date()
         d2 = datetime.strptime(self.last_login, "%Y-%m-%d").date()
 
-        # TESTING TO ENSURE THE FORMAT IS THE SAME; CAN BE DELETED.
-        print("Today's date: {}".format(d1))
-        print("Date of last login: {}".format(d2))
         # FINDING THE TIME DIFFERENCE BETWEEN THE FORMATTED DATES.
         # DELTA WILL OUTPUT THE DIFFERENCE IN DAYS.
         delta = d1 - d2
         self.login_difference = delta.days
-        # TESTING TO ENSURE THE VARIABLE WORKS; CAN BE DELETED.
-        print("Days since last login: {}".format(self.login_difference))
-        return self.login_difference
+
+        return "Days since last login: {}".format(self.login_difference)
 
 
 
@@ -123,9 +123,7 @@ class TheUserStreak:
             now = datetime.now()
             self.last_login = now.strftime("%Y-%m-%d")
 
-            # A CHECK TO ENSURE THAT THE VARIABLE IS WORKING; CAN BE DELETED.
-            print(type(self.last_login))
-            print("TODAY: {}".format(self.last_login))
+            print("TODAY: {}".format(now.strftime("%d-%m-%Y")))
 
             # DB ENGINE
             db_name = 'GOL_users'
@@ -133,22 +131,23 @@ class TheUserStreak:
             db_connection = _connect_to_db(db_name)
             # Cursor
             cur = db_connection.cursor()
-            print("Database connection Successful")
 
             if self.login_difference == 1:
                 print("Thanks for joining today! Your streak goes up!")
                 streaks_query_02 = "UPDATE the_users SET UserStreak = UserStreak + 1, LastLogin = '{DATE}' WHERE {COLUMN} = '{VALUE}'".format(
                     COLUMN=self.column, VALUE=self.value, DATE=self.last_login)
+                return "There has been a streak increase"
 
             elif self.login_difference < 1:
                 print("Keep up the hard work. \nDont forget to join us tomorrow too!")
                 streaks_query_02 = "UPDATE the_users SET UserStreak = {STREAK}, LastLogin = '{DATE}' WHERE {COLUMN} = '{VALUE}'".format(
                     COLUMN=self.column, VALUE=self.value, DATE=self.last_login, STREAK=self.existing_user_streak)
-
+                return "The streak has stayed the same"
             else:
                 print("Nice to see you! It's been a long time.")
                 streaks_query_02 = " UPDATE the_users SET UserStreak = 1, LastLogin = '{DATE}' WHERE {COLUMN} = '{VALUE}'".format(
                     COLUMN=self.column, VALUE=self.value, DATE=self.last_login)
+                return "The streak has been reset"
 
             cur.execute(streaks_query_02)
             db_connection.commit()
@@ -162,32 +161,18 @@ class TheUserStreak:
         finally:
             if db_connection:
                 db_connection.close()
-                print("DB connection closed")
 
 
     def display_user_streak(self):
         db_connection = None
 
         try:
-
-        # NOTE:
-        #     #THE FOLLOWING CODE EXTRACTS THE DAYS DATE AND ASSIGNS IT TO THE VARIABLE AS A STR.
-        #     #THIS ENABLES THE INFORMATION TO BE SENT INTO THE DB WITHOUT THROWING AN ERROR WITH MYSQL SYNTAX.
-            now = datetime.now()
-            self.last_login = now.strftime("%Y-%m-%d")
-
-        # A CHECK TO ENSURE THAT THE VARIABLE IS WORKING; CAN BE DELETED.
-            print(type(self.last_login))
-            print("TODAY: {}".format(self.last_login))
-
-
             # DB ENGINE
             db_name = 'GOL_users'
             # Database engine
             db_connection = _connect_to_db(db_name)
             # Cursor
             cur = db_connection.cursor()
-            print("Database connection Successful")
 
             # THE QUERY CALLS THE USER STREAK NUMBER FROM THE DB, SO THAT IT CAN BE DISPLAYED ON THE INTERFACE.
             streaks_query_03 = "SELECT UserStreak FROM the_users WHERE {COLUMN} = '{VALUE}';".format(
@@ -203,7 +188,6 @@ class TheUserStreak:
             print("Gift of Learning Streak: {} Days.".format(final_streak))
 
             self.existing_user_streak = final_streak
-
             return self.existing_user_streak
 
         except Exception:
@@ -212,23 +196,22 @@ class TheUserStreak:
         finally:
             if db_connection:
                 db_connection.close()
-                print("DB connection closed")
 
 
+# TESTING
 # test_01 = TheUserStreak(1, 'Username', 'cobrien1')
-# test_01.get_last_login()
-# test_01.get_existing_user_streak()
-# test_01.calculate_login_diff()
-# test_01.update_userstreak_and_last_login()
-# test_01.display_user_streak()
-#
-#
-# test_02 = TheUserStreak(3, 'Username', 'hbieber1997')
-# test_02.get_last_login()
-# test_02.get_existing_user_streak()
-# test_02.calculate_login_diff()
-# test_02.update_userstreak_and_last_login()
-# test_02.display_user_streak()
+# print(test_01.get_last_login())
+# print(test_01.get_existing_user_streak())
+# print(test_01.calculate_login_diff())
+# print(test_01.update_userstreak_and_last_login())
+# print(test_01.display_user_streak())
+
+test_02 = TheUserStreak(3, 'Username', 'hbieber1997')
+test_02.get_last_login()
+test_02.get_existing_user_streak()
+test_02.calculate_login_diff()
+test_02.update_userstreak_and_last_login()
+test_02.display_user_streak()
 #
 # test_03 = TheUserStreak(4, 'Username', 'lucosov89')
 # test_03.get_last_login()
