9c078fc49cb583f91e407b80570d98987a65643d
chrissietina92 <56897522+chrissietina92@users.noreply.github.com>
2022-11-02 00:14:35 +0000

    connecting search word db functions to main file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.Login_Interface_Python_Logic import login_interface
 from src.db_functions import new_user_credentials
 from src.daily_words import randomWordGenerator
-from src.db_searched_words import add_searched_word, display_users_searched_word, display_all_searched_words
+from src.db_searched_words import add_searched_word, display_users_searched_word
 from src.dictionaryapi_functions import show_word_and_definition
 import schedule
 import time
@@ -29,23 +29,28 @@ def set_reminder_time():
         print('Please enter the right format of the time HH:MM')
         set_reminder_time()
 
-def learn_words():
+def learn_words(userid):
     start = input('Would you like to search your Schedule your words, Search your Dictionary, or View your Searched Words? (Schedule Word/Search Dictionary/View Searched Words) ').lower()
     if start == 'schedule word':
         set_reminder_time()
 
     elif start == 'search dictionary':
-        search_words_in_dictionary()
-        continue_learning()
+        search_words_in_dictionary(userid)
+        continue_learning(userid)
     elif start == 'view searched words':
-        display_all_searched_words()
-        continue_learning()
+        list_of_searched_words = display_users_searched_word(userid)
+        print("")
+        print("SEARCHED WORDS:")
+        for word in list_of_searched_words:
+            print("{}: {}".format(word[0], word[1]))
+        print("")
+        continue_learning(userid)
     else:
         print('Please try again')
-        continue_learning()
+        continue_learning(userid)
 
 
-def search_words_in_dictionary():
+def search_words_in_dictionary(userid):
     search_again = input('Would you like to continue searching your dictionary? (Yes/No) ').lower()
     if search_again == 'no':
         print('You are leaving your dictionary...')
@@ -53,22 +58,22 @@ def search_words_in_dictionary():
     elif search_again == 'yes':
         search = input('Please search for a word in your dictionary: ')
         show_word_and_definition(search)
-        add_searched_word(search)
-        search_words_in_dictionary()
+        add_searched_word(search, userid)
+        search_words_in_dictionary(userid)
     else:
         print('Please try again.')
-        search_words_in_dictionary()
+        search_words_in_dictionary(userid) #JACK SAID TO GET RID OF THESE RECURSION, MAYBE JUST EXIT PROGRAMME
 
-def continue_learning():
+def continue_learning(userid):
     cont_learning = input('Would you like to continue? (Yes/No) ').lower()
     if cont_learning == 'no':
         print('See you next time.')
         exit(0)
     elif cont_learning == 'yes':
-        learn_words()
+        learn_words(userid)
     else:
         print("Please type 'Yes' or 'No'")
-        continue_learning()
+        continue_learning(userid) #JACK SAID TO GET RID OF THESE RECURSION, MAYBE JUST EXIT PROGRAMME
 
 
 def run():
@@ -85,9 +90,9 @@ def run_user_input():
                             Choice (1/2): """)
     if option == '1':
         login = login_interface()
-        if login:
+        if login[0]:
             print("Let's start learning")
-            learn_words()
+            learn_words(login[1])
 
     elif option == '2':
         print('Please register adding the required information.')
@@ -98,14 +103,18 @@ def run_user_input():
             print("Congratulations! Registration completed.")
             start = input('Would you like to log in with your new account to start learning? (Yes/No)')
         if start == 'Yes':
-            login_interface()
-            print("Let's start learning")
-            learn_words()
+            login = login_interface()
+            if login[0]:
+                print("Let's start learning")
+                learn_words(login[1])
+            else:
+                print('Sorry your log in details were wrong, goodbye')
+                return
         elif start == 'No':
             print('See you next time.')
         else:
-            print('Please try again.')
-            learn_words()
+            print('That was an incorrect input, goodbye')
+
     else:
         print('Please try again. Please select one of the two options.')
         run_user_input()
