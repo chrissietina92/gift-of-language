b89f545e96ca00eaf4cc6e0acef356d4f06b825a
Rebecca Ajayi <112581842+Sc0tch-Bonnet@users.noreply.github.com>
2022-11-03 11:05:09 +0000

    Update gol_users.sql
    Updated DB with time stamp for searched words.

--- src/gol_users.sql
+++ src/gol_users.sql
@@ -1,6 +1,3 @@
--- SQL Code to create database for users
--- RUN THIS CODE SCRIPT IN MYSQL WORKBENCH
-
 CREATE DATABASE GOL_users;
 USE GOL_users;
 
@@ -17,26 +14,34 @@ CREATE TABLE the_users (
 	UserPassword varchar(65),
 	LastLogin DATE,
 	UserStreak Int,
-
+    
 CONSTRAINT
     pk_user_id PRIMARY KEY (UserID));
-
-
+    
+    
     CREATE TABLE searched_words (
 	SearchedWordID int(100) AUTO_INCREMENT,
 	UserID int(10),
     word varchar(50) NOT NULL,
     definition_ varchar(6000) NOT NULL,
+    date_accessed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (SearchedWordID),
     FOREIGN KEY (UserID) REFERENCES the_users (UserID)
     );
 
-
+    
 -- DB VALUES.
--- INITIAL POPULATING THE USERS TABLE
+-- INITIAL POPULATING THE USERS TABLE 
 
 INSERT INTO the_users (FirstName, LastName, Email, DOB, City, Username, UserPassword)
-VALUES ('Jake', 'Callow', 'jcal@email.com', str_to_date('05-12-1992', '%d-%m-%Y'), 'London', 'jcal', 'cat123'),
+VALUES ('Jake', 'Callow', 'jcal@email.com', str_to_date('05-12-1992', '%d-%m-%Y'), 'London', 'cobrien1', 'cat123'),
 ('Fred', 'Smith', 'fsmith@email.com', str_to_date('19-02-1995', '%d-%m-%Y'),'Bristol', 'freddy95', 'bristol11'),
 ('Hayley', 'Bieber', 'hayley99@email.com', str_to_date('01-09-1997', '%d-%m-%Y'), 'Cambridge', 'hbieber1997', 'ilovejustin1'),
 ('Luciano', 'Sovino', 'lucosovino89@email.com', str_to_date('02-10-1989', '%d-%m-%Y'), 'Cardiff', 'lucosov89', 'helloworld0');
+    
+-- POTENTIAL TRIGGER TO BE RAISED FOR DUPLICATE ENTRIES INTO CODE. 
+
+-- DB VALUES
+-- POPULATING USER STREAK/LAST LOGIN
+UPDATE the_users SET UserStreak = 2, LastLogin = '2022-11-01' WHERE FirstName = 'Jake';
+
